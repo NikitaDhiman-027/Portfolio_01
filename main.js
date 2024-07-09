78d4d6ca78c89282b70d9c565098b1eec5bf1d84
Nikita Dhiman <nikudhiman01@gmail.com>
2024-07-10 00:00:42 +0530

    Portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Full-Stack Developer", "Python Programmer", "Web Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
